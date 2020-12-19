a1eb72d27dce395c5b690578e188ec6901b7df97
LeeBongHyeon <11jjklol@naver.com>
2020-12-19 16:27:45 +0900

    don_button

--- To_do_list/To_do_list.js
+++ To_do_list/To_do_list.js
@@ -24,6 +24,12 @@ function addtional()
         }
 
         document.getElementById("input").value = "";
+
+        /* 완료 버튼 */
+        var done_btn = document.createElement("BUTTON");
+        done_btn.id = 'done' + i;
+        done_btn.className="fas fa-check";
+        document.getElementById("list").appendChild(done_btn);
         
         /* 제거 버튼 생성 */
         var remove_btn = document.createElement("BUTTON");
@@ -39,6 +45,10 @@ function addtional()
         br.id= 'br' + i;
         document.getElementById("list").appendChild(br);
         i+=1;
+
+        done_btn.onclick = function(){
+            done(add, remove_btn.id);
+        };
     }
 }
 
@@ -48,13 +58,57 @@ function remove(a)
     var n = document.getElementById('node' + a);
     var bnt = document.getElementById(a);
     var b = document.getElementById('br' + a);
+    var d = document.getElementById('done' + a);
     n.style.display="none";
     bnt.style.display = "none";
     b.style.display = "none";
+    d.style.display = "none";
 }
 
 function modify(a)
 {
     var value = prompt("Modify", "What is your list?");
     document.getElementById(a).innerHTML = value;
+}
+
+function done(a, r)
+{
+    alert("Done!");
+    var node = document.createElement("SPAN");
+    var textnode = document.createTextNode(a);
+    node.id = 'node_d' + r;
+    node.appendChild(textnode);
+    document.getElementById("done").appendChild(node);
+        
+    /* 제거 버튼 생성 */
+    var remove_btn = document.createElement("BUTTON");
+    remove_btn.id = 'bnt' + r;
+    remove_btn.className="fas fa-minus";
+    remove_btn.onclick = function(){
+        remove_d(node.id, remove_btn.id, br.id);
+    };
+
+    document.getElementById("done").appendChild(remove_btn);
+
+    var br = document.createElement("BR");
+    br.id = 'br_d' + r;
+    document.getElementById("done").appendChild(br);
+
+    node.onclick = function(){
+        modify(node.id);
+    }
+
+    remove(r);
+}
+
+function remove_d(n_i, bnt_i, b_i)
+{
+    alert('Remove your list');
+    var n = document.getElementById(n_i);
+    var bnt = document.getElementById(bnt_i);
+    var b = document.getElementById(b_i);
+    n.style.display="none";
+    bnt.style.display = "none";
+    b.style.display = "none";
+    d.style.display = "none";
 }
