8111f64c6d67a57e0b50dfbb7bf0ea7772274317
LeeBongHyeon <11jjklol@naver.com>
2021-01-09 22:01:44 +0900

    another_language

--- speech_text/script.js
+++ speech_text/script.js
@@ -1,8 +1,10 @@
-let data = [
-  ["img/water.jpg", "I'm THIRSTY"], ["img/hungry.jpg", "I'M HUNGRY"], ["img/tired.jpg", "I'M TIRED"],
-  ["img/hurt.jpg", "I'M HURT"], ["img/happy.jpg", "I'M HAPPY"], ["img/angry.jpg", "I'M ANGRY"],
-  ["img/sad.jpg", "I'M SAD"], ["img/scared.jpg", "I'M SCARED"], ["img/outside.jpg", "I WANT TO GO OUTSIDE"],
-  ["img/home.jpg", "I WANT TO GO HOME"], ["img/school.jpg", "I WANT TO GO TO SCHOOL"], ["img/grandma.jpg", "I WANT TO GO TO GRANDMAS"]
+const data = [
+  ["img/water.jpg", "목말라", "我渴了", "喉が渇いた", "I'm THIRSTY"], ["img/hungry.jpg", "배가 고파", "我饿了", "お腹が空きました", "I'M HUNGRY"],
+  ["img/tired.jpg", "피곤해", "我累了", "つかれた", "I'M TIRED"], ["img/hurt.jpg", "상처받았어", "我受伤了", "傷ついた", "I'M HURT"],
+  ["img/happy.jpg", "행복해", "我很快乐", "満足しています", "I'M HAPPY"], ["img/angry.jpg", "화났어", "我生气了", "怒っている", "I'M ANGRY"],
+  ["img/sad.jpg", "슬퍼", "我很难过", "悲しいです", "I'M SAD"], ["img/scared.jpg", "무서워", "我好害怕", "怖いです", "I'M SCARED"],
+  ["img/outside.jpg", "밖에 나가고 싶어", "我想出去", "外に出たい", "I WANT TO GO OUTSIDE"], ["img/home.jpg", "집에 가고 싶어", "我想回家", "家に帰りたい", "I WANT TO GO HOME"],
+  ["img/school.jpg", "학교에 가고 싶어", "我想上学", "学校に行きたい", "I WANT TO GO TO SCHOOL"], ["img/grandma.jpg", "할머니를 만나고 싶어", "我想见见奶奶。", "おばあちゃんに会いたい。", "I WANT TO GO TO GRANDMAS"]
 ];
 
 window.onload = function () {
@@ -15,7 +17,7 @@ function create(i) {
   let main = document.createElement("div")
   main.className = "main";
   main.onclick = function () {
-    speak(data[i][1], select.options[select.selectedIndex].value
+    speak(data[i][select.selectedIndex + 1], select.options[select.selectedIndex].value
     )
   }
 
@@ -24,7 +26,7 @@ function create(i) {
 
   let comment = document.createElement("p");
   comment.className = "comment";
-  comment.append(data[i][1]);
+  comment.append(data[i][4]);
 
   main.appendChild(image);
   main.appendChild(comment);
@@ -37,7 +39,7 @@ function speak(text, language) {
     alert("This browser does not support speech synthesis.")
     return
   }
-  window.speechSynthesis.cancel() // 현재 읽고있다면 초기화
+  window.speechSynthesis.cancel() // 초기화
 
   if(!text)
   {
@@ -54,12 +56,13 @@ function speak(text, language) {
 
 
 // 이벤트 영역
-const select = document.getElementById("select-lang")
+const select = document.getElementById("lang")
 const text = document.getElementById("text")
 const btn = document.getElementById("btn")
 
 btn.addEventListener("click", e => {
   speak(text.value, select.options[select.selectedIndex].value)
+  alert(select.selectedIndex);
 })
 
 function enter() {
