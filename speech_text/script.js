a709d96af802fec252a6329b25d2f80010b85bc7
LeeBongHyeon <11jjklol@naver.com>
2021-01-11 11:17:17 +0900

    explain

--- speech_text/script.js
+++ speech_text/script.js
@@ -1,5 +1,5 @@
 const data = [
-  ["img/water.jpg", "목말라", "我渴了", "喉が渇いた", "I'm THIRSTY"], ["img/hungry.jpg", "배가 고파", "我饿了", "お腹が空きました", "I'M HUNGRY"],
+  ["img/water.jpg", "목말라", "我渴了", "喉が渇いた", "I'm THIRSTY"], ["img/hungry.jpg", "배고프다", "我饿了", "お腹が空きました", "I'M HUNGRY"],
   ["img/tired.jpg", "피곤해", "我累了", "つかれた", "I'M TIRED"], ["img/hurt.jpg", "상처받았어", "我受伤了", "傷ついた", "I'M HURT"],
   ["img/happy.jpg", "행복해", "我很快乐", "満足しています", "I'M HAPPY"], ["img/angry.jpg", "화났어", "我生气了", "怒っている", "I'M ANGRY"],
   ["img/sad.jpg", "슬퍼", "我很难过", "悲しいです", "I'M SAD"], ["img/scared.jpg", "무서워", "我好害怕", "怖いです", "I'M SCARED"],
@@ -7,22 +7,25 @@ const data = [
   ["img/school.jpg", "학교에 가고 싶어", "我想上学", "学校に行きたい", "I WANT TO GO TO SCHOOL"], ["img/grandma.jpg", "할머니를 만나고 싶어", "我想见见奶奶。", "おばあちゃんに会いたい。", "I WANT TO GO TO GRANDMAS"]
 ];
 
+/* 사진 및 글 목록 12개 생성 */
 window.onload = function () {
   for (let i = 0; i < 12; i++) {
     create(i);
   }
 }
 
+/* 사진 및 글 목록 생성 */
 function create(i) {
+
+  /* main이 사진과 글을 감싸는 형태 */
   let main = document.createElement("div")
   main.className = "main";
   main.onclick = function () {
-    speak(data[i][select.selectedIndex + 1], select.options[select.selectedIndex].value
-    )
+    speak(data[i][select.selectedIndex + 1], select.value);     //미리 만들어 둔 data를 이용하여 선택
   }
 
   let image = new Image();
-  image.src = data[i][0];
+  image.src = data[i][0];         //미리 만들어 둔 주소를 이용하여 선택
 
   let comment = document.createElement("p");
   comment.className = "comment";
@@ -34,39 +37,34 @@ function create(i) {
   document.getElementById("container").appendChild(main);
 }
 
-function speak(text, language) {
-  if (typeof SpeechSynthesisUtterance === "undefined" || typeof window.speechSynthesis === "undefined") {
-    alert("This browser does not support speech synthesis.")
-    return
+const select = document.getElementById("lang")    //select 정보
+const text = document.getElementById("text")      //input 정보
+const btn = document.getElementById("btn")        //button 정보
+
+btn.addEventListener("click", e => {
+  speak(text.value, select.value)
+})
+
+/* enter키 누를 시 */
+function enter(event) {
+  if (event.keyCode == 13) {
+    speak(text.value, select.value)
   }
+}
+
+/* 소리 출력 */
+function speak(text, language) {
   window.speechSynthesis.cancel() // 초기화
 
+  /* input이 비어있다면 */
   if(!text)
   {
     text = "Please fill in the blank";
   }
 
   const speechMsg = new SpeechSynthesisUtterance()
-  speechMsg.lang = language;
-  speechMsg.text = text
+  speechMsg.lang = language;      //언어 설정
+  speechMsg.text = text;          //글 설정
 
-  // SpeechSynthesisUtterance에 저장된 내용을 바탕으로 음성합성 실행
-  window.speechSynthesis.speak(speechMsg)
-}
-
-
-// 이벤트 영역
-const select = document.getElementById("lang")
-const text = document.getElementById("text")
-const btn = document.getElementById("btn")
-
-btn.addEventListener("click", e => {
-  speak(text.value, select.options[select.selectedIndex].value)
-  alert(select.selectedIndex);
-})
-
-function enter() {
-  if (event.keyCode == 13) {
-    speak(text.value, select.options[select.selectedIndex].value)
-  }
+  window.speechSynthesis.speak(speechMsg)   //소리 출력
 }
